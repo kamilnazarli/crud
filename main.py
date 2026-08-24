56fab6fa3705af15cce9d6ec8e3ad0b03341848c
Kamil <nezerovk425@gmail.com>
2026-08-24 12:40:33 +0400

    Stage 0: create SQLite database

--- main.py
+++ main.py
@@ -1,6 +1,31 @@
 from fastapi import FastAPI, HTTPException, status
 from pydantic import BaseModel, Field
 from typing import Optional
+import sqlite3
+
+conn = sqlite3.connect("tasks.db")
+
+cursor = conn.cursor()
+
+cursor.execute("""
+    CREATE TABLE IF NOT EXISTS tasks(
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        title TEXT NOT NULL,
+        done BOOL 
+    )
+""")
+cursor.execute("SELECT EXISTS (SELECT 1 FROM tasks)")
+
+is_not_empty = cursor.fetchone()[0]
+if not is_not_empty:
+    cursor.execute("INSERT INTO tasks (title, done) VALUES (?, ?)", ("workout", True))
+    cursor.execute("INSERT INTO tasks (title, done) VALUES (?, ?)", ("programming", False))
+    cursor.execute("INSERT INTO tasks (title, done) VALUES (?, ?)", ("hiking", False))
+    conn.commit()
+    print("It is empty")
+else:
+    print("It is not empty")
+
 
 class Task(BaseModel):
     title: str
@@ -17,9 +42,9 @@ class TaskUpdate(BaseModel):
 
 app = FastAPI()
 
-tasks = [
-    {"id": 1, "title": "workout", "done": False}
-]
+# tasks = [
+#     {"id": 1, "title": "workout", "done": False}
+# ]
 
 @app.get("/")
 async def root():
@@ -29,50 +54,50 @@ async def root():
 async def health():
     return {"status": "ok"}
 
-@app.get("/tasks")
-async def get_tasks():
-    return tasks
+# @app.get("/tasks")
+# async def get_tasks():
+#     return tasks
 
-@app.get("/tasks/{id}")
-async def get_task(id: int):
+# @app.get("/tasks/{id}")
+# async def get_task(id: int):
 
-    for task in tasks:
-        if task["id"] == id:
-            return task
-    raise HTTPException(status_code=404, detail="Unknown id")
+#     for task in tasks:
+#         if task["id"] == id:
+#             return task
+#     raise HTTPException(status_code=404, detail="Unknown id")
 
-@app.post("/tasks", response_model=TaskOut, status_code=status.HTTP_201_CREATED)
-async def add_task(task: Task):
-    if len(task.title) == 0 or len(task.title.strip()) == 0:
-        raise HTTPException(status_code=400, detail="Bad Request")
-    db_user_dict = task.model_dump()
-    new_id = max([i["id"] for i in tasks], default=0) + 1
-    db_user_dict.update({"id": new_id, "done": False})
-    tasks.append(db_user_dict)
-    return db_user_dict
+# @app.post("/tasks", response_model=TaskOut, status_code=status.HTTP_201_CREATED)
+# async def add_task(task: Task):
+#     if len(task.title) == 0 or len(task.title.strip()) == 0:
+#         raise HTTPException(status_code=400, detail="Bad Request")
+#     db_user_dict = task.model_dump()
+#     new_id = max([i["id"] for i in tasks], default=0) + 1
+#     db_user_dict.update({"id": new_id, "done": False})
+#     tasks.append(db_user_dict)
+#     return db_user_dict
 
-@app.put("/tasks/{id}", response_model=TaskOut)
-async def update_task(id: int, task: TaskUpdate):
-    if task.title is None and task.done is None:
-        raise HTTPException(status_code=400, detail="Empty/invalid body")
-    if task.title is not None and len(task.title.strip()) == 0:
-        raise HTTPException(status_code=400, detail="Empty/invalid body")
+# @app.put("/tasks/{id}", response_model=TaskOut)
+# async def update_task(id: int, task: TaskUpdate):
+#     if task.title is None and task.done is None:
+#         raise HTTPException(status_code=400, detail="Empty/invalid body")
+#     if task.title is not None and len(task.title.strip()) == 0:
+#         raise HTTPException(status_code=400, detail="Empty/invalid body")
 
-    for ts in tasks:
-        if ts["id"] == id:
-            if task.title is not None:
-                ts["title"] = task.title
+#     for ts in tasks:
+#         if ts["id"] == id:
+#             if task.title is not None:
+#                 ts["title"] = task.title
     
-            if task.done is not None:
-                ts["done"] = task.done
-            return ts
+#             if task.done is not None:
+#                 ts["done"] = task.done
+#             return ts
 
-    raise HTTPException(status_code=404, detail="Unknown id")
+#     raise HTTPException(status_code=404, detail="Unknown id")
 
-@app.delete("/tasks/{id}", status_code=204)
-async def delete_task(id: int):
-    for ts in tasks:
-        if ts["id"] == id:
-            tasks.remove(ts)
-            return
-    raise HTTPException(status_code=404, detail="Unknown id")
+# @app.delete("/tasks/{id}", status_code=204)
+# async def delete_task(id: int):
+#     for ts in tasks:
+#         if ts["id"] == id:
+#             tasks.remove(ts)
+#             return
+#     raise HTTPException(status_code=404, detail="Unknown id")
